b5890939b7a1dafb5fa705d85fe75c1972264d1b
fayyam1991 <fayyam1991@gmail.com>
2020-03-17 16:04:51 +0530

    [FaHa] Add rest service

---
+++ src/main/java/com/india/springdemo/jaxrs/resources/DetailsServiceResources.java
@@ -0,0 +1,30 @@
+package com.india.springdemo.jaxrs.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.india.springdemo.model.LocationStats;
+import com.india.springdemo.services.CoronaVirusDataService;
+
+/**
+ * 
+ * @author Fayyam
+ *
+ */
+
+@RestController
+public class DetailsServiceResources {
+
+	@Autowired
+	private CoronaVirusDataService service;
+
+	@RequestMapping(value = "info", method = RequestMethod.GET, produces = { "application/json" })
+	public List<LocationStats> getDetailInfo() {
+		return service.getAllStats();
+	}
+
+}
